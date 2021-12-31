f807212da4695b01f97edbf12e5be74126ee35d8
Cagri Avsar <c.avsar@hotmail.com>
2021-12-31 10:02:28 +0100

    added images and started slideshow

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'cagriavsar';
+export class AppComponent implements OnInit{
+  images= ['key.jpg', 'laptop.jpg', 'team.jpg'];
+  currentImage = 0;
+  showImage = true;
+
+
+  
+  ngOnInit() {
+    this.updateImage();
+  }
+
+  updateImage() {
+    setInterval( () => {
+      this.currentImage++
+      this.currentImage = this.currentImage % this.images.length;
+      this.showImage = false;
+
+      setTimeout(() => {
+        this.showImage = true;
+      }, 100);
+    }, 8000)
+  }
 }
